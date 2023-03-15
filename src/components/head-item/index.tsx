9671b06426d255709affb251af9f8d11c29c65b5
jameszow <jameszow@163.com>
2023-03-15 08:31:21 +0800

    remove vue3 framework, choice use React

--- src/components/head-item/index.tsx
+++
@@ -1,8 +0,0 @@
-import { defineComponent } from 'vue';
-import './index.less';
-
-export default defineComponent({
-  setup(_props, { slots }) {
-    return () => <div class="head-item">{slots.default?.()}</div>;
-  },
-});
