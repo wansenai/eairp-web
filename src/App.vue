a575e0dfc3aa58596fd346f96676475ee38c0e0c
jameszow <jameszow@163.com>
2023-09-22 18:05:24 +0800

    Add a new dark mode to synchronize with Vben's new PR

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <ConfigProvider :locale="getAntdLocale">
+  <ConfigProvider :locale="getAntdLocale" :theme="isDark ? darkTheme : {}">
     <AppProvider>
       <RouterView />
     </AppProvider>
@@ -7,15 +7,19 @@
 </template>
 
 <script lang="ts" setup>
-  import { ConfigProvider } from 'ant-design-vue';
-  import { AppProvider } from '@/components/Application';
-  import { useTitle } from '@/hooks/web/useTitle';
-  import { useLocale } from '@/locales/useLocale';
-  import 'dayjs/locale/zh-cn';
+import { ConfigProvider } from 'ant-design-vue';
+import { AppProvider } from '@/components/Application';
+import { useTitle } from '@/hooks/web/useTitle';
+import { useLocale } from '@/locales/useLocale';
 
-  // support Multi-language
-  const { getAntdLocale } = useLocale();
+import 'dayjs/locale/zh-cn';
+import { useDarkModeTheme } from '@/hooks/setting/useDarkModeTheme';
 
-  // Listening to page changes and dynamically changing site titles
-  useTitle();
+// support Multi-language
+const { getAntdLocale } = useLocale();
+
+const { isDark, darkTheme } = useDarkModeTheme();
+
+// Listening to page changes and dynamically changing site titles
+useTitle();
 </script>
