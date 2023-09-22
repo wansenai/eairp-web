dfd7f6162d303396e97b72c6e3caa8df62a26677
jameszow <jameszow@163.com>
2023-09-22 20:33:06 +0800

    Remove eslint-config in internal module CC #21

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
 module.exports = {
   root: true,
-  extends: ['@vben'],
-};
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
