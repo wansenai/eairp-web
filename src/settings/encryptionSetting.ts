66d33eb43c5a26963ac143301af2af64d25d7cca
jameszow <jameszow@163.com>
2023-10-02 15:51:32 +0800

    Update default cache 7 day-> 1 day

--- src/settings/encryptionSetting.ts
+++ src/settings/encryptionSetting.ts
@@ -1,7 +1,7 @@
 import { isDevMode } from '/@/utils/env';
 
 // System default cache time, in seconds
-export const DEFAULT_CACHE_TIME = 60 * 60 * 24 * 7;
+export const DEFAULT_CACHE_TIME = 60 * 60 * 24 * 1;
 
 // aes encryption key
 export const cacheCipher = {
