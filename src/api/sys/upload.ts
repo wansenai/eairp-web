736d8ece6594d7e359e51e2b78bba614f6bf38dc
jameszow <jameszow@163.com>
2023-10-08 09:46:15 +0800

    fix typescript error

--- src/api/sys/upload.ts
+++ src/api/sys/upload.ts
@@ -2,6 +2,7 @@ import { UploadApiResult } from './model/uploadModel';
 import { defHttp } from '/@/utils/http/axios';
 import { UploadFileParams } from '/#/axios';
 import { useGlobSetting } from '/@/hooks/setting';
+import { AxiosProgressEvent } from 'axios';
 
 const { uploadUrl = '' } = useGlobSetting();
 
@@ -9,13 +10,14 @@ const { uploadUrl = '' } = useGlobSetting();
  * @description: Upload interface
  */
 export function uploadApi(
-  params: UploadFileParams,
-  onUploadProgress: (progressEvent: ProgressEvent) => void,
+    params: UploadFileParams,
+    onUploadProgress: (progressEvent: AxiosProgressEvent) => void,
 ) {
-  return defHttp.uploadFile<UploadApiResult>(
-    {
-      url: uploadUrl,
-    },
-    params,
-  );
+    return defHttp.uploadFile<UploadApiResult>(
+        {
+            url: uploadUrl,
+            onUploadProgress,
+        },
+        params,
+    );
 }
